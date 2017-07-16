c9ef934a22da36f6203a2183c26a8ce66232e1ce
Humza Khan <humzakhan@outlook.com>
2017-07-16 19:49:09 +0500

    Add CommonHelper to hold various helper functions

---
+++ Raptor.Lib/Raptor.Core/CommonHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Raptor.Core
+{
+    public static class CommonHelper
+    {
+        /// <summary>
+        /// Verifies that a string is in valid e-mail format
+        /// </summary>
+        /// <param name="email">Email to verify</param>
+        /// <returns>true if the string is a valid e-mail address and false if it's not</returns>
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+
+            email = email.Trim();
+            var result = Regex.IsMatch(email, "^(?:[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+\\.)*[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!\\.)){0,61}[a-zA-Z0-9]?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\\[(?:(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\.){3}(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\]))$", RegexOptions.IgnoreCase);
+            return result;
+        }
+
+        /// <summary>
+        /// Checks if a string is null
+        /// </summary>
+        /// <param name="stringToCheck">The string to verify</param>
+        /// <returns>Whether string is null or not</returns>
+        public static string EnsureNotNull(string stringToCheck)
+        {
+            return stringToCheck ?? string.Empty;
+        }
+
+        /// <summary>
+        /// Ensure that a string doesn't exceed maximum allowed length
+        /// </summary>
+        /// <param name="str">Input string</param>
+        /// <param name="maxLength">Maximum length</param>
+        /// <param name="postfix">A string to add to the end if the original string was shorten</param>
+        /// <returns>Input string if its lengh is OK; otherwise, truncated input string</returns>
+        public static string EnsureMaximumLength(string str, int maxLength, string postfix = null)
+        {
+            if (string.IsNullOrEmpty(str))
+                return str;
+
+            if (str.Length <= maxLength) return str;
+            var pLen = postfix?.Length ?? 0;
+
+            var result = str.Substring(0, maxLength - pLen);
+            if (!string.IsNullOrEmpty(postfix))
+            {
+                result += postfix;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Generate random digit code
+        /// </summary>
+        /// <param name="length">Length</param>
+        /// <returns>Result string</returns>
+        public static string GenerateRandomDigitCode(int length)
+        {
+            var random = new Random();
+            var str = string.Empty;
+            for (var i = 0; i < length; i++)
+                str = string.Concat(str, random.Next(10).ToString());
+            return str;
+        }
+
+    }
+}
