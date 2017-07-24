4a598c3079b8111e11fce4fd7930595b8231a464
Humza Khan <humzakhan@outlook.com>
2017-07-24 21:23:56 +0500

    Add Database Initializer Test

---
+++ Raptor.Tests/Raptor.Tests.Data/Core/DbInitializerTests.cs
@@ -0,0 +1,14 @@
+﻿using Raptor.Data.Core;
+using Xunit;
+
+namespace Raptor.Tests.Data.Core
+{
+    public class DbInitializerTests
+    {
+        [Fact]
+        public void TryInitializingDatabase() {
+            // Should run smoothly wihout any exceptions.
+            DbInitializer.Seed();
+        }
+    }
+}
