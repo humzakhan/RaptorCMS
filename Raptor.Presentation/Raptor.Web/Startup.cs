621e16d38b5bc3c3d044ef6b8c864b24a30a808b
Humza Khan <humzakhan@outlook.com>
2017-07-29 22:18:34 +0500

    Register services with web app

--- Raptor.Presentation/Raptor.Web/Startup.cs
+++ Raptor.Presentation/Raptor.Web/Startup.cs
@@ -3,6 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Raptor.Services.Blog;
+using Raptor.Services.Configuration;
+using Raptor.Services.Content;
+using Raptor.Services.Logging;
+using Raptor.Services.Security;
+using Raptor.Services.Users;
 
 namespace Raptor.Web
 {
@@ -11,6 +17,7 @@ namespace Raptor.Web
         public IConfigurationRoot Configuration { get; }
 
         public Startup(IHostingEnvironment env) {
+            // Build the basic configuration for our web app
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -20,16 +27,26 @@ namespace Raptor.Web
         }
 
         public void ConfigureServices(IServiceCollection services) {
+            // Enable lowercases routes for urls
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            // Add MVC to our web app
             services.AddMvc();
+
+            // Register our services
+            services.AddTransient<IBlogService, BlogService>();
+            services.AddTransient<ISettingService, SettingService>();
+            services.AddTransient<IContentService, ContentService>();
+            services.AddTransient<ICustomerActivityService, CustomerActivityService>();
+            services.AddTransient<ILogService, LogService>();
+            services.AddTransient<IPermissionService, PermissionService>();
+            services.AddTransient<IUserRegisterationService, UserRegistrationService>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-            }
-
+            // Register the routes for areas
             app.UseMvc(routes => {
                 routes.MapRoute("areaRoute", "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
 
@@ -38,6 +55,7 @@ namespace Raptor.Web
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
+            // Select the right error handling pages
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
@@ -45,8 +63,10 @@ namespace Raptor.Web
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            // Allow to serve static files
             app.UseStaticFiles();
 
+            // Register MVC routes
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: "default",
