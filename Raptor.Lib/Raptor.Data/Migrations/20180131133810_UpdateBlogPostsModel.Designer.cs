0b96e6e811acff7033d2765a10c3405f09830d70
Humza Khan <humzakhan@outlook.com>
2018-01-31 18:41:12 +0500

    Update blog posts model

--- Raptor.Lib/Raptor.Data/Migrations/20180131133810_UpdateBlogPostsModel.Designer.cs
+++ Raptor.Lib/Raptor.Data/Migrations/20180131133810_UpdateBlogPostsModel.Designer.cs
@@ -14,9 +14,10 @@ using System;
 namespace Raptor.Data.Migrations
 {
     [DbContext(typeof(RaptorDbContext))]
-    partial class RaptorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180131133810_UpdateBlogPostsModel")]
+    partial class UpdateBlogPostsModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
