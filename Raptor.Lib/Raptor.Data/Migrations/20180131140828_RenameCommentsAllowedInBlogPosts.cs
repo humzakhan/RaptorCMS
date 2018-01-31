e4a2a88124f885fe956d571c710fd4326fff3c79
Humza Khan <humzakhan@outlook.com>
2018-01-31 19:09:13 +0500

    Rename comments allowed column in blog posts model

---
+++ Raptor.Lib/Raptor.Data/Migrations/20180131140828_RenameCommentsAllowedInBlogPosts.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Raptor.Data.Migrations
+{
+    public partial class RenameCommentsAllowedInBlogPosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentsStatus",
+                table: "BlogPosts");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCommentsAllowed",
+                table: "BlogPosts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCommentsAllowed",
+                table: "BlogPosts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CommentsStatus",
+                table: "BlogPosts",
+                nullable: true);
+        }
+    }
+}
