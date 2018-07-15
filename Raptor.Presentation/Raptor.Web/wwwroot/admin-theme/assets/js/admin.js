55bea0eee9b4bdd443a6854d23b9c1cf5a0e4e32
Humza Khan <humzakhan@outlook.com>
2018-07-15 21:35:01 +0500

    Add top bar to main site and admin panel

---
+++ Raptor.Presentation/Raptor.Web/wwwroot/admin-theme/assets/js/admin.js
@@ -0,0 +1,8 @@
+﻿$(window).scroll(function () {
+    if ($(window).scrollTop() >= 50) {
+        $('#adminbar').addClass('sticky');
+    }
+    else {
+        $('#adminbar').removeClass('sticky');
+    }
+});
